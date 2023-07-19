c383caa3e872668bf8773f51c1bbadeb1c4eec96
Matthew WANG <matthewwang.study@gmail.com>
2023-07-19 23:58:11 +0800

    updated ignore

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const appConfig = {
-  mapboxToken:
-    "pk.eyJ1IjoibWF0dGp3YW5nIiwiYSI6ImNsaXB5NDN1cTAzMnAza28xaG54ZWRrMzgifQ.cUju1vqjuW7XmAuO2iEZmg",
-};
-
-export const mapConfig = {
-  carMovingStepsPerTimeInterval: 20,
-};
