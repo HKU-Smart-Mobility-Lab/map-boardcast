651e623ff490a1ff7ec061798baf2d1f1d520c09
Matthew WANG <matthewwang.study@gmail.com>
2023-07-26 15:16:58 +0800

    more actions handling such as orderReceived and cancel

--- src/utils/actionsHandling.js
+++ src/utils/actionsHandling.js
@@ -1,9 +1,16 @@
 import { ActionType } from "../data/actions";
 import { createPulsingDot } from "./passengersHandling";
-import { passengerNaming } from "./naming";
+import { driverNaming, passengerNaming } from "./naming";
 import { DriverStatus, updateDriverStatus } from "./driversHandling";
+import { mapConfig } from "../config";
 
-export const actionsHandling = (map, actionType, data) => {
+export const actionsHandling = (
+  map,
+  actionType,
+  data,
+  currentTime,
+  driverRoutes
+) => {
   switch (actionType) {
     case ActionType.passengerAppear:
       console.log(actionType, data);
@@ -14,7 +21,7 @@ export const actionsHandling = (map, actionType, data) => {
       rangeUpdate(map, data);
       break;
     case ActionType.orderReceived:
-      orderReceived(map, data);
+      orderReceived(map, data, currentTime, driverRoutes);
       break;
     case ActionType.pickUp:
       pickUp(map, data);
@@ -23,6 +30,7 @@ export const actionsHandling = (map, actionType, data) => {
       dropOff(map, data);
       break;
     case ActionType.cancel:
+      cancel(map, data);
       break;
     default:
       console.error("No such action type");
@@ -42,6 +50,11 @@ function passengerAppear(map, data) {
   const { imageName, sourceName, layerName } = passengerNaming(
     passenger.passengerid
   );
+
+  if (map.getLayer(layerName)) map.removeLayer(layerName);
+  if (map.getSource(sourceName)) map.removeSource(sourceName);
+  if (map.hasImage(imageName)) map.removeImage(imageName);
+
   map.addImage(imageName, createPulsingDot(passenger.range, map), {
     pixelRatio: 2,
   });
@@ -106,15 +119,29 @@ function dropOff(map, data) {
       driverid: 17,
     };
   */
+  const { driverPickingUpRouteSourceName, driverPickingUpRouteLayerName } =
+    driverNaming(data.driverid);
   updateDriverStatus(map, data.driverid, DriverStatus.idle);
+  map.removeSource(driverPickingUpRouteSourceName);
+  map.removeLayer(driverPickingUpRouteLayerName);
 }
 
-const cancelAction = {
-  passengerid: 1,
-  driverid: 17,
-};
+function cancel(map, data) {
+  /*
+   * data format: 
+   * {
+      passengerid: 1,
+      driverid: 17,
+    };
+  */
+  const { driverPickingUpRouteSourceName, driverPickingUpRouteLayerName } =
+    driverNaming(data.driverid);
+  updateDriverStatus(map, data.driverid, DriverStatus.idle);
+  map.removeSource(driverPickingUpRouteSourceName);
+  map.removeLayer(driverPickingUpRouteLayerName);
+}
 
-function orderReceived(map, data) {
+function orderReceived(map, data, currentTime, driverRoutes) {
   /*
    * data format: 
    * {
@@ -123,5 +150,39 @@ function orderReceived(map, data) {
       pickUpTime: 30,
     };
   */
+  const { driverPickingUpRouteSourceName, driverPickingUpRouteLayerName } =
+    driverNaming(data.driverid);
+  const startIndex = currentTime;
+  const endIndex = data.pickUpTime;
+
+  const arc = driverRoutes[data.driverid].features[0].geometry.coordinates
+    .slice(startIndex, endIndex * mapConfig.carMovingStepsPerTimeInterval)
+    .reverse();
+
+  const _pickUpRoute = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "LineString",
+          coordinates: arc,
+        },
+      },
+    ],
+  };
+  map.addSource(driverPickingUpRouteSourceName, {
+    type: "geojson",
+    data: _pickUpRoute,
+  });
+  map.addLayer({
+    id: driverPickingUpRouteLayerName,
+    source: driverPickingUpRouteSourceName,
+    type: "line",
+    paint: {
+      "line-width": 2,
+      "line-color": "#007cbf",
+    },
+  });
   updateDriverStatus(map, data.driverid, DriverStatus.drivingToPickup);
 }
