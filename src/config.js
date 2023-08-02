1fba0a715453ea304de25b535ea47de71504391c
Matthew WANG <matthewwang.study@gmail.com>
2023-08-02 21:22:07 +0800

    added config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const appConfig = {
+  mapboxToken:
+    "pk.eyJ1IjoibWF0dGp3YW5nIiwiYSI6ImNsaXB5NDN1cTAzMnAza28xaG54ZWRrMzgifQ.cUju1vqjuW7XmAuO2iEZmg",
+};
+
+export const mapConfig = {
+  carMovingStepsPerTimeInterval: 5,
+  startingLontitude: 114.1686,
+  startingLatitude: 22.28,
+  zoomingLevel: 15,
+};
